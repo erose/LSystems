746a63db87f6f6f51cd799eaa521097af24f17d3
Eli Rose <erose@oberlin.edu>
2015-01-21 18:10:36 -0500

    Transitioned to json.

--- base.py
+++ base.py
@@ -1,28 +1,51 @@
-from turtle import Turtle, mainloop, tracer, update, mode
-import branches
-import time
+from PIL import Image, ImageDraw
+import sys, time, math, json
 
-class DrawingTurtle(Turtle):
+class UndrawnTurtle():
+    def __init__(self, start_x=0.0, start_y=0.0, start_angle=0.0):
+        self.x, self.y, self.angle = start_x, start_y, start_angle
+        self.points_visited = []
+        self._visit()
+
+    def forward(self, distance):
+        angle_radians = math.radians(self.angle)
+
+        self.x += math.cos(angle_radians) * distance
+        self.y += math.sin(angle_radians) * distance
+
+        self._visit()
+
+    def right(self, angle):
+        self.angle -= angle
+
+    def left(self, angle):
+        self.angle += angle
+
+    def goto(self, x, y):
+        self.x, self.y = x, y
+        self._visit()
+
+    def _visit(self):
+        """
+        Add point to the list of points gone to by the turtle.
+        """
+        self.points_visited.append((self.x, self.y))
+
+class DrawingTurtle(UndrawnTurtle):
     def take_state(self, state):
         """
         Assume the position & heading given.
         """
-        x, y, heading = state
-
-        # Speed 0 is instantaneous travel.
-        orig_speed = self.speed()
-        self.speed(0)
+        x, y, angle = state
 
         self.goto(x, y)
-        self.setheading(heading)
-
-        self.speed(orig_speed)
+        self.angle = angle
 
     def get_state(self):
         """
         Returns the current position & heading as a tuple.
         """
-        return self.xcor(), self.ycor(), self.heading()
+        return self.x, self.y, self.angle
 
 def replace(s, rules):
     """
@@ -34,9 +57,8 @@ def draw(s, turt, distance, left_angle, right_angle):
     """
     Draws string s with the given turtle.
     """
-    start_time = time.time()
 
-    # A stack of x, y, heading tuples.
+    # A stack of x, y, angle tuples.
     stack = []
 
     for char in s:
@@ -46,22 +68,25 @@ def draw(s, turt, distance, left_angle, right_angle):
         if char == "[": stack.append(turt.get_state())
         if char == "]": turt.take_state(stack.pop())
 
-    print("Finished drawing", time.time() - start_time)
-
 
 if __name__ == "__main__":
-    mode("logo")
-    t = DrawingTurtle()
-    t.hideturtle()
-    tracer(0)
+    width, height = 600, 600
+    t = DrawingTurtle(width/2, height, -90)
 
-    s = branches.start
-    print("How many iterations?")
-    for _ in range(int(input())):
-        s = replace(s, branches.rules)
+    L_system = json.loads(open(sys.argv[1]).read())
 
-    print("Finished iterating.")
-    draw(s, t, branches.distance, branches.left_angle, branches.right_angle)
-    update()
+    s = L_system["start_str"]
+    for _ in range(int(input("How many iterations? "))):
+        s = replace(s, L_system["rules"])
 
-    mainloop()
+    distance = L_system["distance"]
+    left_angle = L_system["left_angle"]
+    right_angle = L_system["right_angle"]
+    draw(s, t, distance, left_angle, right_angle)
+
+    canvas = Image.new('RGB', (width, height), color='white')
+    canvas_draw = ImageDraw.Draw(canvas)
+
+    canvas_draw.line(t.points_visited, (0, 0, 0))
+    canvas.show()
+    # canvas.save(sys.argv[2], format="gif")
--- branches.py
+++
@@ -1,7 +0,0 @@
-distance = 20
-left_angle, right_angle = 45, 25
-
-start = "F"
-rules = {
-    "F": "F[-F]F[+F][F]"
-    }
